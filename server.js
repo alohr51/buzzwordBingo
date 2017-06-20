408d1cb1c93a1c6069b65d1006b95cca2c5aafd1
Andrew J. Lohr <ajlohr@us.ibm.com>
2017-06-20 15:25:09 -0400

    formatting

--- server.js
+++ server.js
@@ -10,40 +10,35 @@ var path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
 var buzzwords = ["High-Value","Cloud","Scalability","Bluemix","PaaS","Big Data", "REST","SaaS","Watson","Git","Liberty","Node.js", "Asynchronous",
-              "Multi-Threaded", "Virtualization", "Analytics", "Internet of Things", "DevOps", "Mobile", "Innovation", "Framework", "buildpack", "Runtime", "boilerplate",
-              "Services","Integration", "Security" ];
+"Multi-Threaded", "Virtualization", "Analytics", "Internet of Things", "DevOps", "Mobile", "Innovation", "Framework", "buildpack", "Runtime", "boilerplate",
+"Services","Integration", "Security" ];
 
 // socket.io listen for messages
-io.on('connection', function(socket) {  
-  socket.on('init', function() {        
-    socket.emit('init', buzzwords);        
-  });
-
+io.on('connection', function(socket) {
+	// send out the buzzwords to each connected client
+	socket.emit('init', buzzwords);
 });
 
 //Fisher-Yates (Knuth) Shuffle
 function randomize(array){
-  var currentIndex = array.length, temporaryValue, randomIndex ;
+	var currentIndex = array.length, temporaryValue, randomIndex ;
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
 
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
-  // Pick a remaining element...
-  randomIndex = Math.floor(Math.random() * currentIndex);
-  currentIndex -= 1;
-
-  // And swap it with the current element.
-  temporaryValue = array[currentIndex];
-  array[currentIndex] = array[randomIndex];
-  array[randomIndex] = temporaryValue;
-  }
-
-  return array;
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+	return array;
 }
 
 // handle HTTP GET request to the "/" URL
 app.get('/', function(req, res) {
-  res.render('index.html');
+	res.render('index.html');
 });
 
 // The IP address of the Cloud Foundry DEA (Droplet Execution Agent) that hosts this application:
